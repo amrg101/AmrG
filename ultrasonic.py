e7e0361169dba24476e1551e547e215a32d10723
khalidwaleed0 <khalidwaleed0@outlook.com>
2020-12-30 17:46:57 +0200

    Fixed button problems
    couldn't be checked because they are just checked in part of a second instead of 2 seconds.

--- ultrasonic.py
+++ ultrasonic.py
@@ -3,12 +3,11 @@ import Adafruit_DHT
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
 
 theft_buzzer = 11
-button = 12
+stopTheftButton = 12
 fire_buzzer = 13
-button_2 = 15
+stopFireButton = 15
 trig = 16
 echo = 18
 dhtSensor = Adafruit_DHT.DHT11
@@ -20,7 +19,7 @@ fireExist = False
 
 def triggerUltraSonic():
     GPIO.output(trig, False)
-    time.sleep(2)
+    time.sleep(0.1)
     GPIO.output(trig, True)
     time.sleep(0.00001)
     GPIO.output(trig, False)
@@ -31,25 +30,21 @@ def measureDistance():
     while GPIO.input(echo)==1:
         pulseEnd = time.time()
     pulseDuration = pulseEnd - pulseStart
-    distance = int(pulseDuration) * 17150
+    distance = int(pulseDuration * 17150)
     return distance
 
 def checkTemp():
-    while True:
-        humidity , temperature = Adafruit_DHT.read_retry(dhtSensor, dhtPin)
-        if temperature is not None:
-            if temperature > 50:
-                GPIO.output(fire_buzzer,1)
-                fireExist = True
-            else:
-                GPIO.output(fire_buzzer,0)
-                fireExist = False
+    humidity , temperature = Adafruit_DHT.read_retry(dhtSensor, dhtPin)
+    if temperature is not None:
+        if temperature > 50:
+            GPIO.output(fire_buzzer,1)
+            fireExist = True
 
 def check_ButtonStates():
-    if (GPIO.input(button)==True):
+    if (GPIO.input(stopTheftButton)==True):
         GPIO.output(theft_buzzer,0)
         theftExist = False
-    if (GPIO.input(button_2) == True):
+    if (GPIO.input(stopFireButton) == True):
         GPIO.output(fire_buzzer ,0)
         fireExist = False
 
@@ -68,24 +63,23 @@ def setupPins():
     GPIO.setup(trig,GPIO.OUT)
     GPIO.setup(echo,GPIO.IN)
     GPIO.setup(theft_buzzer,GPIO.OUT)
-    GPIO.setup(button,GPIO.IN)
+    GPIO.setup(stopTheftButton,GPIO.IN)
     GPIO.setup(fire_buzzer,GPIO.OUT)
-    GPIO.setup(button_2 , GPIO.IN)
+    GPIO.setup(stopFireButton , GPIO.IN)
     GPIO.setup(redLed,GPIO.OUT)
     GPIO.setup(greenLed, GPIO.OUT)
 
 setupPins()
 while True:
-    
     triggerUltraSonic()
     distance = measureDistance()
     if distance < 100:
-        print(distance,"cm")
         GPIO.output(theft_buzzer,1)
         theftExist = True
     checkTemp()
-    check_ButtonStates()
-    check_LedStates()
-    time.sleep(2)
-
+    currentTime = time.time()
+    endTime = currentTime + 2
+    while endTime < time.time():    # waits for 2 seconds and at the same time checks the states
+        check_ButtonStates()        # this couldn't be done if we used time.sleep()
+        check_LedStates()
 GPIO.cleanup()	
